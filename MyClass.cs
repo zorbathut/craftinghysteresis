50a69623a1c1dfe5d930a9ef1446922e625e426c
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2016-08-20 14:35:42 -0700

    Put together the prototype framework.

---
+++ MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: zorba
+ * Date: 2016-08-20
+ * Time: 2:15 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace CraftingHysteresis
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
